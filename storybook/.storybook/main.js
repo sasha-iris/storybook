fc170231f94ff28a7e3a9320cbdd4b905accf730
sasha-iris <sasha@irisfinance.co>
2026-06-03 06:16:38 +0300

    Fix main.js: remove docs folder reference

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/html-vite').StorybookConfig } */
 const config = {
-  stories: ['../stories/**/*.stories.js', '../docs/**/*.mdx'],
+  stories: ['../stories/**/*.stories.js'],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/html-vite',
